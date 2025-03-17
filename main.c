fde980f48dd6fa85e95d7bdf805f5fcfd4fdd168
Enriquepardo <quiquepardos@gmail.com>
2025-03-17 16:28:27 +0100

    Finished

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: enpardo- <enpardo-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/28 10:57:43 by enpardo-          #+#    #+#             */
-/*   Updated: 2025/03/16 20:37:10 by enpardo-         ###   ########.fr       */
+/*   Updated: 2025/03/17 16:24:18 by enpardo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,9 @@ int	main(void)
 {
 	int count_ft, count_std;
 	// Prueba con una cadena larga y múltiples argumentos
-	count_ft = ft_printf("Largo: %s %d %x %p %c\n", "Cadena larga...", 123456,
+	count_ft = ft_printf("Largo: %s %d %x %p %c\n", "Cadena larga...", 1234567,
 			0xABCDEF, NULL, 'Z');
-	count_std = printf("Largo: %s %d %x %p %c\n", "Cadena larga...", 123456,
+	count_std = printf("Largo: %s %d %x %p %c\n", "Cadena larga...", 1234567,
 			0xABCDEF, NULL, 'Z');
 	printf("ft_printf: %d, printf: %d\n", count_ft, count_std);
 	return (0);
--- src/ft_printf.c
+++ src/ft_printf.c
@@ -6,18 +6,9 @@
 /*   By: enpardo- <enpardo-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/28 10:56:19 by enpardo-          #+#    #+#             */
-/*   Updated: 2025/03/03 10:07:03 by enpardo-         ###   ########.fr       */
+/*   Updated: 2025/03/17 16:10:43 by enpardo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-/*
-Si count fuera una variable normal (int count = 0;),
-cada función recibiría una copia de count en la pila,
-lo que significaría que los cambios no afectarían al valor original.
-Al usar un puntero (int *count),
-todas las funciones trabajan sobre la misma dirección de memoria,
-asegurando que el valor acumulado se mantenga en cada llamada.
-*/
 
 #include "ft_printf.h"
 
